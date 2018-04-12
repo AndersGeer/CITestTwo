57548b058dc7625a0e3f87ded7802e6cb90f0ecd
Anders Geer <geer.anders@gmail.com>
2018-04-12 17:39:58 +0200

    Include a failing test

--- Assets/Editor/EditModeTests.cs
+++ Assets/Editor/EditModeTests.cs
@@ -6,14 +6,20 @@ using System.Collections;
 
 public class EditModeTests {
 
-	[Test]
-	public void EditModeTestsSimplePasses() {
-		// Use the Assert class to test conditions.
-	}
+    [Test]
+    public void EditModeTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions.
+    }
+    [Test]
+    public void EditModeTestsSimplePassesFail()
+    {
+        Assert.True(false);
+    }
 
-	// A UnityTest behaves like a coroutine in PlayMode
-	// and allows you to yield null to skip a frame in EditMode
-	[UnityTest]
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
 	public IEnumerator EditModeTestsWithEnumeratorPasses() {
 		// Use the Assert class to test conditions.
 		// yield to skip a frame
