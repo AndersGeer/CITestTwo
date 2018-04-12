67951fa6081a7150491ec89df75ace33c7081af2
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-12 23:37:56 +0200

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,6 +5,19 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="ci-build"
 
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -testResults $(pwd)/unityTests.xml \
+  -testPlatform editmode \
+  -runTests \
+  -quit									
+
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
