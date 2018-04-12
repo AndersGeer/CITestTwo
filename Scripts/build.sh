8e41998bbac5559b2b8c4df1129ed3deb345af59
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-12 19:16:59 +0200

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,17 +7,12 @@ project="ci-build"
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log\
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -testResults $(pwd)/unityTests.xml \
-  -testPlatform editmode \
-  -runTests\
-  -quit
+ -runTests 
+ -projectPath $(pwd) 
+ -testResults $(pwd)/unityTests.xml 
+ -testPlatform editmode 
+ 
 
 ls /Users/travis/build/flaps16/CITestTwo/
 echo 'Logs from build'
-cat $(pwd)/unity.log
+cat $(pwd)/unityTests.xml
