2332dab31c904b3fa3073991f6c0bef229d1af20
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-16 11:15:02 +0200

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,32 +7,34 @@ project="ci-build"
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -accept-apiupdate \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unityWin.log \
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -editorTestsResultFile $(pwd)/unityTestsWin.log \
   -testResults $(pwd)/unityTestsWin.xml \
   -testPlatform editmode \
-  -runTests \
-  -quit									
+  -runEditorTests					
 
 echo 'Logs from windows build'
 cat $(pwd)/unityWin.log
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -accept-apiupdate \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -editorTestsResultFile $(pwd)/unityTests.log \
   -testResults $(pwd)/unityTests.xml \
   -testPlatform editmode \
-  -runTests \
-  -quit
+  -runEditorTests
 
 ls /Users/travis/build/flaps16/CITestTwo/
 echo 'Logs from build'
