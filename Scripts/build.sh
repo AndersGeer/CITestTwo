45cb3592e15fd130acd74184b3f70a41074b2715
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-17 10:30:13 +0200

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,37 +6,13 @@
 project="ci-build"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -accept-apiupdate \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unityWin.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -editorTestsResultFile $(pwd)/unityTestsWin.log \
-  -testResults $(pwd)/unityTestsWin.xml \
-  -testPlatform editmode \
-  -runEditorTests \
-  -runTests \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unityWin.log -projectPath $(pwd) -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -editorTestsResultFile $(pwd)/unityTestsWin.log -testResults $(pwd)/unityTestsWin.xml -testPlatform editmode -runEditorTests -runTests
 
 echo 'Logs from windows build'
 cat $(pwd)/unityWin.log
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -accept-apiupdate \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -editorTestsResultFile $(pwd)/unityTests.log \
-  -testResults $(pwd)/unityTests.xml \
-  -testPlatform editmode \
-  -runEditorTests \
-  -runTests \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -accept-apiupdate -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" -editorTestsResultFile $(pwd)/unityTests.log -testResults $(pwd)/unityTests.xml -testPlatform editmode -runEditorTests -runTests
 
 ls /Users/travis/build/flaps16/CITestTwo/
 echo 'Logs from build'
